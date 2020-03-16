6acedbfbcc7cd825804d30f5147d5ab3e1628ace
clemoseitano <clemoseitano@outlook.com>
2020-03-16 13:13:09 +0000

    Using material components

--- lib/main.dart
+++ lib/main.dart
@@ -1,91 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyAppBar extends StatelessWidget {
-  MyAppBar({this.title});
-
-  // Fields in a Widget subclass are always marked "final".
-
-  final Widget title;
-
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Container(
-        height: 56.0, // in logical pixels
-        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
-        decoration: BoxDecoration(color: Colors.blue[900]),
-        // Row is a horizontal, linear layout.
-        child: Row(
-          // <Widget> is the type of items in the list.
-          children: <Widget>[
-            IconButton(
-              icon: Icon(Icons.menu),
-              tooltip: 'Navigation menu',
-              onPressed: null, // null disables the button
-            ),
-            // Expanded expands its child to fill the available space.
-            Expanded(
-              child: title,
-            ),
-            IconButton(
-              icon: Icon(Icons.search),
-              tooltip: 'Search',
-              onPressed: null,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Flutter Tutorial',
+    home: TutorialHome(),
+  ));
 }
 
-class MyScaffold extends StatelessWidget {
+class TutorialHome extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // Material is a conceptual piece of paper on which the UI appears.
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Example title',
-              style: Theme.of(context).primaryTextTheme.title,
-            ),
-          ),
-          Expanded(
-            child: Center(
-              child: Text('Hello, world!'),
-            ),
+    // Scaffold is a layout for the major Material Components.
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.menu),
+          tooltip: 'Navigation menu',
+          onPressed: null,
+        ),
+        title: Text('Example title'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            tooltip: 'Search',
+            onPressed: null,
           ),
         ],
       ),
+      // body is the majority of the screen.
+      body: Center(
+        child: Text('Hello, world!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add', // used by assistive technologies
+        child: Icon(Icons.add),
+        onPressed: null,
+      ),
     );
   }
-}
-
-void main() {
-  runApp(
-      MaterialApp(
-    debugShowCheckedModeBanner: false,
-    theme: ThemeData(
-      // Define the default brightness and colors.
-      brightness: Brightness.dark,
-      primaryColor: Colors.lightBlue[900],
-      accentColor: Colors.cyan[600],
-
-      // Define the default font family.
-      fontFamily: 'Georgia',
-
-      // Define the default TextTheme. Use this to specify the default
-      // text styling for headlines, titles, bodies of text, and more.
-      textTheme: TextTheme(
-        headline: TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-        title: TextStyle(fontSize: 36.0, fontStyle: FontStyle.italic),
-        body1: TextStyle(fontSize: 14.0, fontFamily: 'Hind'),
-      ),
-    ),
-    title: 'My app', // used by the OS task switcher
-    home: MyScaffold(),
-  ));
 }
